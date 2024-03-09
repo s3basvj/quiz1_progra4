903b3d01e3647311638d3365e3b61482c9400cd1
Sebas Vargas <vargastatan02@gmail.com>
2024-03-09 12:48:01 -0600

    add all quiz 1

---
+++ README.md
@@ -0,0 +1,24 @@
+Quiz 1
+Creación de página web de una municipalidad que brinda el servicio de permisos municipales.
+El sitio web consta de 3 páginas, el landing page que es el inicio de la página donde se explica brevemente de que trata el sitio
+La segunda es la página guía del formulario en esta se explica cómo debe rellenarse el el formulario.
+Tercero, es el formulario cómo tal con los espacios a rellenar y un botón para enviar el formulario dicho botón verifica que los espacios estén completos,
+tiene una alerta si sucede lo contrario y también notifica si se envía correctamente.
+
+Antes de comenzar, asegúrate de tener instalado lo siguiente en tu sistema:
+
+- Node.js: [Descargar e Instalar Node.js](https://nodejs.org)
+- npm: Normalmente se instala automáticamente junto con Node.js
+
+## Instalación
+
+1. Clona este repositorio en tu máquina local:
+
+2. instala las dependencias del proyecto con npm install
+
+3. ejecuta con live server.
+
+## Netlify
+
+Puedes usar y ver el proyecto en el siguiente link:
+
---
+++ script.js
@@ -0,0 +1,44 @@
+const formIds = ['personalDataForm', 'projectDetailsForm', 'termsAndConditionsForm'];
+
+function submitForm() {
+    formIds.forEach(formId => {
+        const form = document.getElementById(formId);
+        const inputs = form.querySelectorAll('input, textarea, select');
+
+        // Verificar si el formulario está completo
+        const isFormComplete = Array.from(inputs).every(input => input.value.trim() !== '');
+
+        if (!isFormComplete) {
+            inputs.forEach(input => {
+                if (input.value.trim() === '') {
+                    input.classList.add('invalid'); // Agregar clase Bootstrap para resaltar
+                } else {
+                    input.classList.remove('is-invalid'); // Eliminar la clase Bootstrap si el campo es válido
+                }
+            });
+
+            appendAlert('Por favor, completa todos los campos.', 'danger');
+
+            return;
+        }
+        appendAlert('El formulario se ha enviado correctamente.', 'success');
+        return;
+        // Código adicional para enviar el formulario
+    });
+}
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = [
+          `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+          `   <div>${message}</div>`,
+          '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+          '</div>'
+        ].join('');
+    
+        alertPlaceholder.innerHTML = ''; // Limpiar alertas previas
+        alertPlaceholder.append(wrapper);
+        const closeBtn = wrapper.querySelector('.btn-close');
+        closeBtn.addEventListener('click', () => wrapper.remove());
+}
